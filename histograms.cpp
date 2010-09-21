02a12ba539265dbaf1e5da201399f3946ff561dd
benbelly <benbelly@c3643d77-5941-4ca3-9a90-978904f1a040>
2010-09-21 17:00:23 +0000

    Corrected histogram image generation. Refactored histogram code. Histograms look good.
    git-svn-id: http://benbelly.unfuddle.com/svn/benbelly_ms/epenthesis@46 c3643d77-5941-4ca3-9a90-978904f1a040

--- histograms.cpp
+++ histograms.cpp
@@ -6,6 +6,10 @@
 #include "histograms.h"
 #include "logging.h"
 
+static const unsigned int Tvert = 9, Thorz = 9;
+typedef std::pair<cv::Range, cv::Range> RowColRange;
+
+
 std::vector<cv::Mat> generateHandHistograms( Frame f, ContourSet cs ) {
     std::vector<cv::Mat> hs; hs.reserve( cs.size() );
     std::transform( cs.begin(), cs.end(), std::back_inserter( hs ),
@@ -13,54 +17,39 @@ std::vector<cv::Mat> generateHandHistograms( Frame f, ContourSet cs ) {
     return hs;
 }
 
-static const unsigned int Tvert = 7, Thorz = 7;
-
-std::pair< cv::Range, cv::Range> bbox( cv::Point p, int hmax, int vmax ) {
-    return std::pair<cv::Range, cv::Range>(
+RowColRange bbox( cv::Point p, int hmax, int vmax ) {
+    return RowColRange(
             cv::Range( std::max( 0, (int)(p.y - Tvert ) ),
                        std::min( vmax, (int)(p.y + Tvert ) ) ),
             cv::Range( std::max( 0, (int)(p.x - Thorz ) ),
                        std::min( hmax, (int)(p.x + Thorz ) ) ) );
-                       
 }
 
-/*
- * for b:bins
- *  for p:points
- *      if( dv( c, p ) < Tv[b] and
- *          dh( c, p ) < Th[b] )
- *              ++hist[b]
- */
+double incrementBox( cv::Mat &hist, const RowColRange &box ) {
+    // There should be some way to stick this in hist?
+    //cv::Mat incedBox = hist( box.first, box.second ) + cv::Scalar( 1 );
+    double total = 0.0;
+    for( int y = box.first.start; y <= box.first.end; ++y ) {
+        for( int x = box.second.start; x <= box.second.end; ++x ) {
+            ++hist.at<double>( y, x );
+            ++total;
+        }
+    }
+    return total;
+}
+
 cv::Mat generateHandHistogram( Frame f, Contour c ) {
     cv::Mat hist = cv::Mat::zeros( f.size(), CV_64FC1 );
     cv::Point center( hist.cols / 2, hist.rows / 2 );
     double total = 0.0;
     Contour::iterator cbegin = c.begin(), cend = c.end();
     for( Contour::iterator p = cbegin; p != cend; ++p ) {
-        std::pair<cv::Range, cv::Range> box = bbox( *p, hist.rows, hist.cols );
-        // There should be some way to stick this in hist?
-        //cv::Mat incedBox = hist( box.first, box.second ) + cv::Scalar( 1 );
-
-        for( int y = box.first.start; y <= box.first.end; ++y ) {
-            for( int x = box.second.start; x <= box.second.end; ++x ) {
-                cv::Point bin( x, y );
-                // no need to check that we're in bounds here - box is defined
-                // to be in bounds
-                ++hist.at<double>( bin );
-                ++total;
-            }
-        }
+        total += incrementBox( hist, bbox( *p, hist.rows, hist.cols ) );
     }
-    double *histBegin = hist.ptr<double>();
-    double *histEnd = histBegin + ( hist.rows * hist.cols );
-    if( total > 0.0 ) { // in case nothing was found?
-        for( double *i = histBegin; i != histEnd; ++i ) {
-            *i = *i / total;
-        }
-    }
-    else {
-        cerr << "Failed to place any points in histogram" << endl;
-    }
+    // Normalize to sum to 1
+    double *histBegin = hist.ptr<double>(),
+           *histEnd = histBegin + ( hist.rows * hist.cols );
+    for( double *i = histBegin; i != histEnd; ++i ) *i = *i / total;
     return hist;
 }
 
@@ -68,12 +57,13 @@ cv::Mat h2i( Frame f, HistogramSet &hs ) {
     cv::Mat dst = cv::Mat::zeros( f.size(), image_types::gray );
     for( HistogramSet::iterator i = hs.begin(); i != hs.end(); ++i ) {
         double max = *(std::max_element( i->begin<double>(), i->end<double>() ) );
-        double scale = 255 / max;
+        double scale = 255. / max;
         double *iBegin = i->ptr<double>(),
-                        *iEnd = iBegin + i->rows * i->cols;
+               *iEnd = iBegin + i->rows * i->cols;
         unsigned char *d = dst.data;
         while( iBegin != iEnd ) {
-            *d = (unsigned char)(*iBegin * scale);
+            if( *iBegin > 0.0 )
+                *d = (unsigned char)(*iBegin * scale);
             ++iBegin; ++d;
         }
     }
